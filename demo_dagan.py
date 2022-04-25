e4180826f62bcd3b00e65115bba8e44d35115731
harlanhong <758558048@qq.com>
2022-04-25 11:25:40 +0800

    force

--- demo_dagan.py
+++ demo_dagan.py
@@ -6,10 +6,19 @@
 import torch
 import torch.nn.functional as F
 import os
+from skimage import img_as_ubyte
+import cv2
 import argparse
+import imageio
+from skimage.transform import resize
 from scipy.spatial import ConvexHull
 from tqdm import tqdm
 import numpy as np
+import modules.generator as G
+import modules.keypoint_detector as KPD
+import yaml
+from collections import OrderedDict
+import depth
 parser = argparse.ArgumentParser(description='Test DaGAN on your own images')
 parser.add_argument('--source_image', default='./temp/source.jpg', type=str, help='Directory of input source image')
 parser.add_argument('--driving_video', default='./temp/driving.mp4', type=str, help='Directory for driving video')
@@ -62,6 +71,7 @@ def find_best_frame(source, driving, cpu=False):
             frame_num = i
     return frame_num
 
+
 def make_animation(source_image, driving_video, generator, kp_detector, relative=True, adapt_movement_scale=True, cpu=False):
     sources = []
     drivings = []
@@ -111,88 +121,88 @@ def make_animation(source_image, driving_video, generator, kp_detector, relative
             predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
             depth_gray.append(gray_driving)
     return sources, drivings, predictions,depth_gray
-# with open("config/vox-adv-256.yaml") as f:
-#     config = yaml.load(f)
-# generator = G.SPADEDepthAwareGenerator(**config['model_params']['generator_params'],**config['model_params']['common_params'])
-# config['model_params']['common_params']['num_channels'] = 4
-# kp_detector = KPD.KPDetector(**config['model_params']['kp_detector_params'],**config['model_params']['common_params'])
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+with open("config/vox-adv-256.yaml") as f:
+    config = yaml.load(f)
+generator = G.SPADEDepthAwareGenerator(**config['model_params']['generator_params'],**config['model_params']['common_params'])
+config['model_params']['common_params']['num_channels'] = 4
+kp_detector = KPD.KPDetector(**config['model_params']['kp_detector_params'],**config['model_params']['common_params'])
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-# g_checkpoint = torch.load("generator.pt", map_location=device)
-# kp_checkpoint = torch.load("kp_detector.pt", map_location=device)
+g_checkpoint = torch.load("generator.pt", map_location=device)
+kp_checkpoint = torch.load("kp_detector.pt", map_location=device)
 
-# ckp_generator = OrderedDict((k.replace('module.',''),v) for k,v in g_checkpoint.items())
-# generator.load_state_dict(ckp_generator)
-# ckp_kp_detector = OrderedDict((k.replace('module.',''),v) for k,v in kp_checkpoint.items())
-# kp_detector.load_state_dict(ckp_kp_detector)
+ckp_generator = OrderedDict((k.replace('module.',''),v) for k,v in g_checkpoint.items())
+generator.load_state_dict(ckp_generator)
+ckp_kp_detector = OrderedDict((k.replace('module.',''),v) for k,v in kp_checkpoint.items())
+kp_detector.load_state_dict(ckp_kp_detector)
 
-# depth_encoder = depth.ResnetEncoder(18, False)
-# depth_decoder = depth.DepthDecoder(num_ch_enc=depth_encoder.num_ch_enc, scales=range(4))
-# loaded_dict_enc = torch.load('encoder.pth')
-# loaded_dict_dec = torch.load('depth.pth')
-# filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in depth_encoder.state_dict()}
-# depth_encoder.load_state_dict(filtered_dict_enc)
-# ckp_depth_decoder= {k: v for k, v in loaded_dict_dec.items() if k in depth_decoder.state_dict()}
-# depth_decoder.load_state_dict(ckp_depth_decoder)
-# depth_encoder.eval()
-# depth_decoder.eval()
+depth_encoder = depth.ResnetEncoder(18, False)
+depth_decoder = depth.DepthDecoder(num_ch_enc=depth_encoder.num_ch_enc, scales=range(4))
+loaded_dict_enc = torch.load('encoder.pth')
+loaded_dict_dec = torch.load('depth.pth')
+filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in depth_encoder.state_dict()}
+depth_encoder.load_state_dict(filtered_dict_enc)
+ckp_depth_decoder= {k: v for k, v in loaded_dict_dec.items() if k in depth_decoder.state_dict()}
+depth_decoder.load_state_dict(ckp_depth_decoder)
+depth_encoder.eval()
+depth_decoder.eval()
     
-# # device = torch.device('cpu')
-# # stx()
+# device = torch.device('cpu')
+# stx()
 
-# generator = generator.to(device)
-# kp_detector = kp_detector.to(device)
-# depth_encoder = depth_encoder.to(device)
-# depth_decoder = depth_decoder.to(device)
+generator = generator.to(device)
+kp_detector = kp_detector.to(device)
+depth_encoder = depth_encoder.to(device)
+depth_decoder = depth_decoder.to(device)
 
-# generator.eval()
-# kp_detector.eval()
-# depth_encoder.eval()
-# depth_decoder.eval()
+generator.eval()
+kp_detector.eval()
+depth_encoder.eval()
+depth_decoder.eval()
 
-# img_multiple_of = 8
+img_multiple_of = 8
 
-# with torch.inference_mode():
-#     if torch.cuda.is_available():
-#         torch.cuda.ipc_collect()
-#         torch.cuda.empty_cache()
-#     source_image = imageio.imread(args.source_image)
-#     reader = imageio.get_reader(args.driving_video)
-#     fps = reader.get_meta_data()['fps']
-#     driving_video = []
-#     try:
-#         for im in reader:
-#             driving_video.append(im)
-#     except RuntimeError:
-#         pass
-#     reader.close()
+with torch.inference_mode():
+    if torch.cuda.is_available():
+        torch.cuda.ipc_collect()
+        torch.cuda.empty_cache()
+    source_image = imageio.imread(args.source_image)
+    reader = imageio.get_reader(args.driving_video)
+    fps = reader.get_meta_data()['fps']
+    driving_video = []
+    try:
+        for im in reader:
+            driving_video.append(im)
+    except RuntimeError:
+        pass
+    reader.close()
 
-#     source_image = resize(source_image, (256, 256))[..., :3]
-#     driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
+    source_image = resize(source_image, (256, 256))[..., :3]
+    driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
 
 
 
-#     i = find_best_frame(source_image, driving_video)
-#     print ("Best frame: " + str(i))
-#     driving_forward = driving_video[i:]
-#     driving_backward = driving_video[:(i+1)][::-1]
-#     sources_forward, drivings_forward, predictions_forward,depth_forward = make_animation(source_image, driving_forward, generator, kp_detector, relative=True, adapt_movement_scale=True, cpu=False)
-#     sources_backward, drivings_backward, predictions_backward,depth_backward = make_animation(source_image, driving_backward, generator, kp_detector, relative=True, adapt_movement_scale=True, cpu=False)
-#     predictions = predictions_backward[::-1] + predictions_forward[1:]
-#     sources = sources_backward[::-1] + sources_forward[1:]
-#     drivings = drivings_backward[::-1] + drivings_forward[1:]
-#     depth_gray = depth_backward[::-1] + depth_forward[1:]
+    i = find_best_frame(source_image, driving_video)
+    print ("Best frame: " + str(i))
+    driving_forward = driving_video[i:]
+    driving_backward = driving_video[:(i+1)][::-1]
+    sources_forward, drivings_forward, predictions_forward,depth_forward = make_animation(source_image, driving_forward, generator, kp_detector, relative=True, adapt_movement_scale=True, cpu=False)
+    sources_backward, drivings_backward, predictions_backward,depth_backward = make_animation(source_image, driving_backward, generator, kp_detector, relative=True, adapt_movement_scale=True, cpu=False)
+    predictions = predictions_backward[::-1] + predictions_forward[1:]
+    sources = sources_backward[::-1] + sources_forward[1:]
+    drivings = drivings_backward[::-1] + drivings_forward[1:]
+    depth_gray = depth_backward[::-1] + depth_forward[1:]
 
-#     imageio.mimsave(args.output, [np.concatenate((img_as_ubyte(s),img_as_ubyte(d),img_as_ubyte(p)),1) for (s,d,p) in zip(sources, drivings, predictions)], fps=fps)
-#     imageio.mimsave("gray.mp4", depth_gray, fps=fps)
-#     # merge the gray video
-#     animation = np.array(imageio.mimread(args.output,memtest=False))
-#     gray = np.array(imageio.mimread("gray.mp4",memtest=False))
+    imageio.mimsave(args.output, [np.concatenate((img_as_ubyte(s),img_as_ubyte(d),img_as_ubyte(p)),1) for (s,d,p) in zip(sources, drivings, predictions)], fps=fps)
+    imageio.mimsave("gray.mp4", depth_gray, fps=fps)
+    # merge the gray video
+    animation = np.array(imageio.mimread(args.output,memtest=False))
+    gray = np.array(imageio.mimread("gray.mp4",memtest=False))
 
-#     src_dst = animation[:,:,:512,:]
-#     animate = animation[:,:,512:,:]
-#     merge = np.concatenate((src_dst,gray,animate),2)
-#     imageio.mimsave(args.output, merge, fps=fps)
+    src_dst = animation[:,:,:512,:]
+    animate = animation[:,:,512:,:]
+    merge = np.concatenate((src_dst,gray,animate),2)
+    imageio.mimsave(args.output, merge, fps=fps)
 
     # print(f"\nRestored images are saved at {out_dir}")
